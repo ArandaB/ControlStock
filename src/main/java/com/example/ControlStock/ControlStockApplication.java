de2fec60b9652786b63bf127e723e806701f6bb5
ArandaB <woldefo@gmail.com>
2024-02-20 18:29:08 -0300

    primeros pasos

---
+++ src/main/java/com/example/ControlStock/ControlStockApplication.java
@@ -0,0 +1,13 @@
+package com.example.ControlStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlStockApplication.class, args);
+	}
+
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,10 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.*;
+@Entity
+@Table(name="user")
+@Data
+public class User {
+
+}
